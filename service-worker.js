9d500645159c30f7dcf58488b22e328b3052bca1
ajudaanapolis-dev <ajudaanapolis@gmail.com>
2026-07-04 15:09:47 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="mll-complete-v1";
-const ASSETS=["./", "./index.html", "./css/styles.css", "./js/app.js", "./data/letters.json", "./data/leo-labs.json", "./manifest.webmanifest", "./assets/icons/icon.svg", "./assets/illustrations/professor-lume.svg", "./assets/illustrations/professor-lume-aponta.svg", "./assets/audio/tap.wav", "./assets/audio/open.wav", "./assets/audio/success.wav", "./assets/audio/wrong.wav", "./assets/audio/magic.wav", "./assets/audio/water.wav", "./assets/audio/animal.wav", "./assets/audio/music.wav", "./assets/audio/science.wav"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
