db20fb894d00d521688a777126b3343cf686b502
ajudaanapolis-dev <ajudaanapolis@gmail.com>
2026-07-04 13:23:52 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-
-const CACHE = "mundo-leo-lucas-v1";
+const CACHE = "mundo-leo-lucas-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +7,7 @@ const ASSETS = [
   "./js/data.js",
   "./js/audio.js",
   "./js/storage.js",
+  "./data/progress.default.json",
   "./manifest.webmanifest",
   "./assets/icons/icon.svg",
   "./assets/audio/click.wav",
@@ -27,13 +27,21 @@ self.addEventListener("install", event=>{
 
 self.addEventListener("activate", event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event=>{
   event.respondWith(
-    caches.match(event.request).then(cached=>cached || fetch(event.request))
+    fetch(event.request)
+      .then(response=>{
+        const copy = response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
   );
 });
